ad7887ba51f851ec8268ebd135e307ca8e60b0e9
Marcos V. Monteiro <98278090+MontMarcos@users.noreply.github.com>
2025-11-08 11:56:37 -0300

    rotas

--- app/controller/aplication.py
+++ app/controller/aplication.py
@@ -3,13 +3,12 @@ from flask import render_template
 class Aplication:
     def __init__(self):
         self.pages = {
-            'principal': self.render_inicio,
+            'principal': self.render_principal,
         }
 
     def render(self, page):
-        content_function = self.pages.get(page, self.render_casamento)
+        content_function = self.pages.get(page, self.render_principal) 
         return content_function()
     
-    def render_inicio(self):
-        return render_template('inicio.html')
-
+    def render_principal(self):
+        return render_template('principal.html')
--- app/run.py
+++ app/run.py
@@ -1,13 +1,13 @@
-from app.controller.aplication import Aplication
+from controller.aplication import Aplication
 from flask import Flask
 
 aplication = Aplication()
-app = Flask(__name__, templete_folder='views')
+app = Flask(__name__, template_folder='views')
 
 @app.route('/')
 @app.route('/', methods=['GET'])
 def principal():
-    return aplication.render('inicio')
+    return aplication.render('principal') 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
