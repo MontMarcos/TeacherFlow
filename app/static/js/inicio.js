5c1fbba9b84d8912ff39d936115b8d91b2c2114a
Marcos V. Monteiro <98278090+MontMarcos@users.noreply.github.com>
2025-11-09 17:52:22 -0300

    Refactor code structure for improved readability and maintainability

--- app/static/js/inicio.js
+++
@@ -1,8 +0,0 @@
-const botaoElemento = document.getElementById('botao');
-
-if (botaoElemento) {
-    // Adiciona uma função que será executada ao clicar no botão
-    botaoElemento.addEventListener('click', function() {
-        alert('Você clicou no botão!');
-    });
-}
